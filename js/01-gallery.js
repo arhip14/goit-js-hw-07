975861a2abc3169982d1a26b3233043f8fff6390
arhip14 <kruvdyk1503@gmail.com>
2023-06-10 11:58:48 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const markup = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+function createGalleryItems(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
+
+galleryContainer.addEventListener('click', onClick);
+
+function onClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImageSrc = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageSrc}" width="800" height="800">
+  `);
+
+  instance.show();
+
+  const keyPressOnEsc = function (event) {
+    const ESC_KEY_CODE = 'Escape';
+    if (event.code === ESC_KEY_CODE) {
+      instance.close();
+      window.removeEventListener('keydown', keyPressOnEsc);
+    }
+  };
+
+  window.addEventListener('keydown', keyPressOnEsc);
+}
+
+
+
