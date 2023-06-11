a41f7c8f4b36ac1a257cacd2cd57ba1e5b7b39f0
arhip14 <kruvdyk1503@gmail.com>
2023-06-11 13:18:07 +0300

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryList(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryList(items) {
+  return items.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `).join('');
+}
+
+gallery.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+
+  const { target } = event;
+
+  if (!target.classList.contains("gallery__image")) {
+    return;
+  }}
+
+let modalImg = new SimpleLightbox('.gallery a', {
+  doubleTapZoom: '1.5',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
